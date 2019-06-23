db1df53596be8af78cb317d9e600ae276bec2491
NAquila <niklas.renstrom@gmail.com>
2019-06-23 16:11:30 +0200

    Added setup.py to git

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='liker',
+      version='1.0',
+      description='Bot that likes pictures on IG',
+      author='Niklas Renström',
+      author_email='niklas.renstrom@gmail.com',
+      licence='Closed source',
+      packages=['liker']
+     )
