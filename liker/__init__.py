53ced1547def706c28d7912e63b70a167773520a
NAquila <niklas.renstrom@gmail.com>
2019-06-24 21:14:02 +0200

    Updated init-file with better log-placement
    Also no cluttering of namespace.

--- liker/__init__.py
+++ liker/__init__.py
@@ -1,18 +1,32 @@
-import yaml
-from os.path import dirname, join
-from logging.config import dictConfig
-from datetime import datetime
-from os.path import expanduser, join
 
-module_file = dirname(__file__)
-logging_file = join(module_file, 'logging_config.yaml')
-credentials_file = join(module_file, 'credentials.yaml')
 
-# Set-up logging
-with open(logging_file, 'r') as f:
-    log_conf = yaml.safe_load(f)
+# Do necessary imports in a function to avoid cluttering up
+# namespace
+def init_logging():
+    import yaml
+    from logging.config import dictConfig
+    from datetime import datetime
+    from os.path import dirname, join, expanduser
+    from os import makedirs
 
-log_filepath = join(expanduser('~'), logs, liker)
-log_filepath += datetime.now().strftime('y/m/d/hm') + '.log'
-log_conf['handlers']['h_file']['filename'] = log_filepath
-dictConfig(log_conf)    
+    module_file = dirname(__file__)
+    logging_file = join(module_file, 'logging_config.yaml')
+    with open(logging_file, 'r') as f:
+        log_conf = yaml.safe_load(f)
+    log_filepath = join(expanduser('~'), 'logs', 'liker')
+    log_filepath += datetime.now().strftime('/%y/%m/%d/%H_%M_%S%f.log')
+    log_dir = dirname(log_filepath)
+    makedirs(log_dir, exist_ok=True)
+    log_conf['handlers']['h_file']['filename'] = log_filepath
+    dictConfig(log_conf)
+
+
+def get_cred_file():
+    from os.path import dirname, join
+    module_file = dirname(__file__)
+    credentials_file = join(module_file, 'credentials.yaml')
+    return credentials_file
+
+
+init_logging()
+credentials_file = get_cred_file()
