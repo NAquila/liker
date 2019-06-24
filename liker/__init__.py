f9d0de6d715f9d4ed2ae2a1bba95a34c2d1c0381
Niklas Renström <niklas.renstrom@gmail.com>
2019-06-24 19:49:11 +0100

    Changes to specify logging

--- liker/__init__.py
+++ liker/__init__.py
@@ -1,6 +1,8 @@
 import yaml
 from os.path import dirname, join
 from logging.config import dictConfig
+from datetime import datetime
+from os.path import expanduser, join
 
 module_file = dirname(__file__)
 logging_file = join(module_file, 'logging_config.yaml')
@@ -9,4 +11,8 @@ credentials_file = join(module_file, 'credentials.yaml')
 # Set-up logging
 with open(logging_file, 'r') as f:
     log_conf = yaml.safe_load(f)
+
+log_filepath = join(expanduser('~'), logs, liker)
+log_filepath += datetime.now().strftime('y/m/d/hm') + '.log'
+log_conf['handlers']['h_file']['filename'] = log_filepath
 dictConfig(log_conf)    
